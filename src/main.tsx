c9bbf646d744e4851e27403017018eca1ff04d29
FoXfIr <lovetemmo@gmail.com>
2023-06-20 16:32:12 +0200

    Refactor code use ReactQuery

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,24 @@
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import App from './App'
-import theme from './theme'
 import './index.css'
+import theme from './theme'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
-)
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement)
+  .render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider theme={theme}>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <App />
+          <ReactQueryDevtools />
+        </ChakraProvider>
+      </QueryClientProvider>
+    </React.StrictMode>,
+  )
