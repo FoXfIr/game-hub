4590021434ffa6b29106e5eaf8b5494799e24fa4
FoXfIr <lovetemmo@gmail.com>
2023-06-22 12:29:24 +0200

    import fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import theme from './theme'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom'
+import './index.css'
 import routes from './routes'
+import theme from './theme'
 
 const queryClient = new QueryClient();
 
